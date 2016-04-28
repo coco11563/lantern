e8db16cb150e7bcec328043db88fcdc8ebb1a1fe
fffw <fffw@getlantern.org>
2016-04-28 17:15:15 +0800

    get feed through Lantern in integration test

--- src/github.com/getlantern/lantern/feed_test.go
+++ src/github.com/getlantern/lantern/feed_test.go
@@ -1,6 +1,8 @@
 package lantern
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -23,25 +25,35 @@ func (retriever *TestFeedRetriever) AddFeed(title, description,
 
 }
 
-func getFeed(t *testing.T, locale string) {
+func getFeed(t *testing.T, locale string, proxyAddr string) {
 	provider := &TestFeedProvider{}
-	GetFeed(locale, "", provider)
-
-	if assert.NotNil(t, CurrentFeed()) {
-		assert.NotEqual(t, 0, NumFeedEntries(),
-			"No feed entries after processing")
-	}
+	GetFeed(locale, proxyAddr, provider)
 
 	feed := CurrentFeed()
+	if !assert.NotNil(t, feed) {
+		return
+	}
+	if !assert.NotEqual(t, 0, NumFeedEntries(),
+		"No feed entries after processing") {
+		return
+	}
 
 	assert.Equal(t, NumFeedEntries(), len(feed.Items["all"]),
 		"All feed items should be equal to total entries")
 }
 
 func TestGetFeed(t *testing.T) {
-
+	tmpDir, err := ioutil.TempDir("", "testconfig")
+	if !assert.NoError(t, err, "Unable to create temp configDir") {
+		return
+	}
+	defer os.RemoveAll(tmpDir)
+	result, err := Start(tmpDir, 5000)
+	if !assert.NoError(t, err, "Should have been able to start lantern") {
+		return
+	}
 	locales := []string{"en_US", "fa_IR", "invalid"}
 	for _, l := range locales {
-		getFeed(t, l)
+		getFeed(t, l, result.HTTPAddr)
 	}
 }
